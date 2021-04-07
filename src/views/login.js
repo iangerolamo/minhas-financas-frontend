979c25eab6528c8d0656c3050a1efd834043d63f
Ian Gerolamo <iangerolamo@gmail.com>
2021-04-07 16:09:55 -0300

    Adicionando estado e comportamento do componente Login

--- src/views/login.js
+++ src/views/login.js
@@ -1,7 +1,18 @@
+import { useState } from 'react';
+
 import Card from '../components/card';
 import FormGroup from '../components/form-group';
 
 function Login() {
+
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  function entrar() {
+    console.log("Email: ", {email})
+    console.log("Senha: " ,{senha})
+  }
+
   return (
     <div className="container">
       <div className="row">
@@ -15,6 +26,8 @@ function Login() {
                       <FormGroup label="Email: *" htmlFor="exampleInputEmail1">
                         <input 
                           type="Email"
+                          value={email}
+                          onChange={e => setEmail(e.target.value)}
                           className="form-control"
                           id="exampleInputEmail1"
                           aria-describedby="emailHelp"
@@ -24,11 +37,15 @@ function Login() {
                       <FormGroup label="Senha: *" htmlFor="exampleInputPassword1">
                         <input 
                           type="password"
+                          value={senha}
+                          onChange={e => setSenha(e.target.value)}
                           className="form-control"
                           id="exampleInputPassword1"
                           placeholder="Password"
                         />
                       </FormGroup>
+                      <button onClick={entrar} className="btn btn-success">Entrar</button>
+                      <button className="btn btn-danger">Cadastrar</button>
                     </fieldset>
                   </div>
                 </div>
