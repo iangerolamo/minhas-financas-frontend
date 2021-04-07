410858d518ee9e0de03bb956d7f8ab7682ebe3dc
Ian Gerolamo <iangerolamo@gmail.com>
2021-04-07 14:48:24 -0300

    Instalando e testando o Bootswatch/Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,37 +1,9 @@
-import { useState } from 'react';
+import 'bootswatch/dist/flatly/bootstrap.css';
 
 function App() {
-
-  const [number1, setNumber1] = useState(0);
-  const [number2, setNumber2] = useState(0);
-  const [total, setTotal] = useState(0);
-
- function calculateTotal() {
-   setTotal(number1 + number2);
- }
-
   return (
     <div>
-      <label>First number: </label>
-        <input 
-          type="number" 
-          value={number1}
-          onChange={e => setNumber1(+e.target.value)}
-          
-        />
-      <br />
-      <label>Second number: </label>
-        <input 
-          type="number"
-          value={number2}
-          onChange={e => setNumber2(+e.target.value)}
-        />
-      <br />
-      <button onClick={calculateTotal}>
-        Add them!
-      </button>
-      
-      <h2>{total}</h2>
+      <button type="button" className="btn btn-primary">Primary</button>
     </div>
   );
 }
