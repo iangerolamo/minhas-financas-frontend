98d908928cf73426b15493692bb96f5639cbc9b8
iangerolamo <iangerolamo@gmail.com>
2021-04-14 12:38:33 -0300

    Criando o componente Select Menu(combo box)

--- src/views/consulta-lancamentos.js
+++ src/views/consulta-lancamentos.js
@@ -2,12 +2,30 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import Card from "../components/card";
 import FormGroup from "../components/form-group";
+import SelectMenu from "../components/selectMenu";
 
 function ConsultaLancamentos() {
+
+  const lista = [
+    { label: 'Selecione...', value: '' },
+    { label: 'Janeiro', value: 1 },
+    { label: 'Fevereiro', value: 2 },
+    { label: 'Março', value: 3 },
+    { label: 'Abril', value: 4 },
+    { label: 'Maio', value: 5 },
+    { label: 'Junho', value: 6 },
+    { label: 'Julho', value: 7 },
+    { label: 'Agosto', value: 8 },
+    { label: 'Setembro', value: 9 },
+    { label: 'Outubro', value: 10 },
+    { label: 'Novembro', value: 11 },
+    { label: 'Dezembro', value: 12 }
+  ];
+
     return (
         <Card title={"Consulta Lançamentos"}>
           <div className={"row"}>
-            <div className={"col-md-12"}>
+            <div className={"col-md-6"}>
               <div className={"bs-component"}>
                 <FormGroup htmlFor={"inputAno"} label={"Ano: *"}>
                   <input type={"text"}
@@ -15,7 +33,9 @@ function ConsultaLancamentos() {
                          id={"inputAno"}
                          aria-describedby={"emailHelp"}
                          placeholder={"Digite o Ano"} />
-
+                </FormGroup>
+                <FormGroup htmlFor="inputMes" label="Mês: ">
+                  <SelectMenu className={"form-control"} lista={lista}/>
                 </FormGroup>
 
               </div>
